91701ee06b747edb869d315c86b26682d757647d
Marcus Vine <marcus.vine@uoit.net>
2020-02-01 08:25:37 -0500

    Simple follower camera

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    public Transform transform;
+    public Transform playerTransform;
+
+    public float cameraOffsetX;
+    public float cameraOffsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        temp.x += cameraOffsetX;
+        temp.y += cameraOffsetY;
+        transform.position = temp;
+
+    }
+}
