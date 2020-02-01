62f3570c7469d0b18839a4d1ca3633873850bb5a
Marcus Vine <marcus.vine@uoit.net>
2020-02-01 14:38:03 -0500

    Zooming with terminal

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -11,22 +11,34 @@ public class CameraBehaviour : MonoBehaviour
     public float cameraOffsetX;
     public float cameraOffsetY;
 
+    public float zoomSpeed = 1;
+    public float targetOrtho = 5.0f;
+    public float smoothSpeed = 2.0f;
+    public float minOrtho = 1.0f;
+    public float maxOrtho = 20.0f;
+
+    public bool transitioning = false;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
-    void LateUpdate()
+    private void Update()
     {
 
-        Vector3 temp = transform.position;
-        temp.x = playerTransform.position.x;
-        temp.y = playerTransform.position.y;
+        if (!transitioning)
+        {
 
-        temp.x += cameraOffsetX;
-        temp.y += cameraOffsetY;
-        transform.position = temp;
+            Vector3 temp = transform.position;
+            temp.x = playerTransform.position.x;
+            temp.y = playerTransform.position.y;
+
+            temp.x += cameraOffsetX;
+            temp.y += cameraOffsetY;
+            transform.position = temp;
+        }  
+       
 
     }
 }
