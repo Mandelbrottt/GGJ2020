0d9709d824c1688428b0c8e06b3ff86ca2c355d0
Mat <mathew.kostrzewa@ontariotechu.net>
2020-02-02 12:40:26 -0500

    Tile grid moving WIP

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-
-    public Transform transform;
     public Transform playerTransform;
 
     public float cameraOffsetX;
     public float cameraOffsetY;
+
+    public bool isFrozenOnOrigin = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +19,24 @@ public class CameraBehaviour : MonoBehaviour
 
     void LateUpdate()
     {
+        if (isFrozenOnOrigin)
+            transform.position = new Vector3(0.0f, 0.0f, -50.0f);
+        else
+        {
+            Vector3 newPos = transform.position;
+            newPos.x += Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+            newPos.y += Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
 
-        Vector3 temp = transform.position;
-        temp.x = playerTransform.position.x;
-        temp.y = playerTransform.position.y;
+            transform.position = newPos;
 
-        temp.x += cameraOffsetX;
-        temp.y += cameraOffsetY;
-        transform.position = temp;
+            //Vector3 temp = transform.position;
+            //temp.x = playerTransform.position.x;
+            //temp.y = playerTransform.position.y;
+
+            //temp.x += cameraOffsetX;
+            //temp.y += cameraOffsetY;
+            //transform.position = temp;
+        }
 
     }
 }
